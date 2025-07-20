d5c652125d33a2e921cdc988be65a093fc6c307e
Rohan Fernando <rohandensilf@gmail.com>
2025-07-20 14:38:20 +0530

    Added templates.
    Added templates, a module to replace and grab data from them, and modified main a little.

---
+++ src/main.py
@@ -0,0 +1,9 @@
+
+# VMDL string that will be worked on, saved, and cleared for the next one
+vmdl = """"""
+# String containing all RenderMeshLists that will be appended to, and placed into vmdl
+render_mesh_lists = """"""
+# String containing all Material Remaps that will be appende to, and placed into MaterialGroupList
+material_group_remaps = """"""
+# String containing the MaterialGroupList template, that will have the Remaps inserted into it.
+material_group_list = """"""
---
+++ src/template_grabber.py
@@ -0,0 +1,55 @@
+"""
+Template Grabber Module
+
+This module:
+  - is used for grabbing text templates from text files
+  - is used to replace parts of templates before returning them
+
+-RF
+"""
+
+import os
+
+# constants, file paths
+VMDL_TMP = "../templates/vmdl_template.txt"
+MGL_TMP = "../templates/materialgrouplist_template.txt"
+MGLR_TMP = "../templates/materialgrouplist_remap_template.txt"
+RML_TMP = "../templates/rendermeshlist_template.txt"
+
+
+# Functions that grab data from templates, then returns it. Some will replace with parameters.
+# Raw VMDL Template
+def grab_vmdl():  
+    with open(
+        os.path.join(os.path.dirname(__file__), VMDL_TMP), 'r'
+        ) as f_vmdl:
+        text = f_vmdl.read()
+        return text
+
+# Material Group List
+def grab_mgl():
+    with open(
+        os.path.join(os.path.dirname(__file__), MGL_TMP), 'r'
+        ) as f_mgl:
+        text = f_mgl.read()
+        return text
+
+# Material Group List Remap
+def grab_mglr(material_from, material_to):
+    with open(
+        os.path.join(os.path.dirname(__file__), MGL_TMP), 'r'
+        ) as f_mgl:
+        text = f_mgl.read()
+        text.replace("{REPLACE_TO_ADD_DEFAULT}", material_from)
+        text.replace("{REPLACE_TO_ADD_VMAT}", material_to)
+        return text
+
+# Render Mesh List
+def grab_rml(filename, scale):
+    with open(
+        os.path.join(os.path.dirname(__file__), RML_TMP), 'r'
+        ) as f_rml:
+        text = f_rml.read()
+        text.replace("{REPLACE_TO_ADD_DMXFBX}", filename)
+        text.replace("{REPLACE_TO_ADD_SCALE}", float(scale))
+        return text
