c51cf6c411f953968c74d5f50ba58eb1ab4cacd8
Douran 2.0 <memeboy.1709@gmail.com>
2025-07-20 00:31:14 -0700

    Create shared_formats.py

---
+++ src/shared_formats.py
@@ -0,0 +1,13 @@
+import os
+import subprocess #this is used for run dmxconvert
+import fbx
+import re #this is used to read the dmx
+
+def check_file():
+    pass
+
+def run_dmxconvert():
+    pass
+
+def init_fbx():
+    pass
