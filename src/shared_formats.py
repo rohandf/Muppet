b653327de2285c2a8655c854ac2b86edb696f3a6
Douran 2.0 <memeboy.1709@gmail.com>
2025-07-20 00:45:40 -0700

    Update shared_formats.py

--- src/shared_formats.py
+++ src/shared_formats.py
@@ -3,11 +3,11 @@ import subprocess #this is used for run dmxconvert
 import fbx
 import re #this is used to read the dmx
 
-def check_file():
+def init_fbx(): #always have this first to intialzied fbx
+    pass
+
+def scan_files():
     pass
 
 def run_dmxconvert():
-    pass
-
-def init_fbx():
     pass
