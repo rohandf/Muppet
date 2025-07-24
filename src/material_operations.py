d70a88342b4228c2c9d1b76d2141f43eb8f05798
Rohan Fernando <rohandensilf@gmail.com>
2025-07-24 13:22:59 +0530

    removed unnecessary loops in mat ops

--- src/material_operations.py
+++ src/material_operations.py
@@ -18,7 +18,7 @@ def add_mats(mat_dict) -> str:
         remaps = remaps + tmp_remap
     return(make_mat_class(remaps))
 
-# places all remaps into materialgrouplist class, called automatically after add_mats
+# places all remaps into materialgrouplist class, called automatically after add_mats. final step
 def make_mat_class(remaps) -> str:
     final_mgl = tg.grab_mgl()
     final_mgl = final_mgl.replace("{REPLACE_TO_ADD_REMAPS}", remaps)
@@ -35,13 +35,20 @@ def make_remap_dict(f_mats,t_mats) -> dict:
             print(result[1])
         else: # if no valid matches:
             pass # do nothing
-    add_mats(mat_dict)
+    return(add_mats(mat_dict))
 
-# call this to start the chain. returns a MaterialGroupList class.
-def get_mats_from_dir(md_dir, vmat_dir) -> str:
-    files = shared_formats.find_files(md_dir, [".dmx",".fbx"], verify=True)
-    f_mats = []
-    for file in files:
-        f_mats = shared_formats.extract_fbx_materials(file)
+
+def get_mats_from_dir(mesh_dir: str, vmat_dir: list) -> str:
+    '''
+    Call this to start the chain. returns a MaterialGroupList class. \n
+    mesh_dir = single model. \n
+    vmat_dir = list of vmats.
+    '''
     t_mats = shared_formats.find_files(vmat_dir, [".vmat"])
-    return(make_remap_dict(f_mats,t_mats)) 
+    f_mats = []
+    if mesh_dir.endswith(".dmx"):
+        f_mats = shared_formats.extract_dmx_materials(mesh_dir)
+    if mesh_dir.endswith(".fbx"):
+        f_mats = shared_formats.extract_fbx_materials(mesh_dir)
+    #return the MaterialGroupList.
+    return(make_remap_dict(f_mats,t_mats))
